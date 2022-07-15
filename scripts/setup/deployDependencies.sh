5396f69195810fd4ade361461e4bf786d986cd87
initdotd <initdotd@hotmail.com>
2022-07-14 21:41:31 -0500

    Auto deploy to dev and int and package.

--- scripts/setup/deployDependencies.sh
+++ scripts/setup/deployDependencies.sh
@@ -2,6 +2,6 @@
 
 echo "Deploying dependencies... This can take some time and terminal will wait!"
 echo "Installing Apex Mocks..."
-sfdx force:package:install -p 04t3a000000PmYgAAK -k keaper -w 20
+sfdx force:package:install -p 04t3a000000PmZAAA0 -k keaper -w 20
 
 ./scripts/setup/configureScratchOrg.sh
